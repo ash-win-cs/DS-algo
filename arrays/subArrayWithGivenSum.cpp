e513c3f161a1181dee606e7c7393dac307a5af9a
ashwin <ashwinc@gmail.com>
2020-12-06 17:25:27 +0530

    Arrays Challenge - Smallest Positive Missing Number

--- arrays/subArrayWithGivenSum.cpp
+++ arrays/subArrayWithGivenSum.cpp
@@ -1,3 +1,19 @@
+/*
+Arrays Challenge-Subarray with given sum
+(Google, Amazon, Facebook, Visa)
+
+Problem
+Given an unsorted array A of size N of non-negative integers, find a continuous
+subarray which adds to a given number S.
+Constraints
+1 <= N <= 105
+0 <= Ai <= 1010
+Example
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+*/
 #include<iostream>
 using namespace std;
 
